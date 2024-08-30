402ce98e51a436f02ee474ee758804cced967205
Alevir Coelho Neto <alevirramos@gmail.com>
2024-08-30 17:13:13 -0300

    Add tags to sections in the site menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -14,3 +14,26 @@ menuMobile.addEventListener('click', () => {
 overlayMenu.addEventListener('click', () => {
     menuMobile.classList.remove('abrir-menu-mobile')
 })
+
+/* marcação da seção*/
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                navLinks.forEach((link) => {
+                    link.classList.remove('active');
+                    if (link.getAttribute('href').substring(1) === entry.target.id) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+    },
+    { threshold: 0.6 } // Ajuste o valor do threshold conforme necessário
+);
+
+sections.forEach((section) => observer.observe(section));
